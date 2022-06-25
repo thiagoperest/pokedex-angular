b14d1f7f84f2829b8f6725c908225a261cda0168
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-06-25 13:10:48 -0300

    Pokeapi

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,15 +1,15 @@
+import { PokeApiService } from './../../service/poke-api.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
-  styleUrls: ['./poke-list.component.scss']
+  styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
-
-  constructor() { }
+  constructor(private pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe((res) => console.log(res));
   }
-
 }
