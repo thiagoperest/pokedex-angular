350b0e3ddd24497346e324eabca2dd41caed70d5
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-07-02 16:05:41 -0300

    Projeto Finalizado

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -16,7 +16,7 @@ export class PokeApiService {
       tap((res) => res),
       tap((res) => {
         res.results.map((resPokemons: any) => {
-          this.apiGetPokemons(resPokemons.url).subscribe(
+          this.apiGetPokemon(resPokemons.url).subscribe(
             (res) => (resPokemons.status = res)
           );
         });
@@ -24,7 +24,7 @@ export class PokeApiService {
     );
   }
 
-  public apiGetPokemons(url: string): Observable<any> {
+  public apiGetPokemon(url: string): Observable<any> {
     return this.http.get<any>(url).pipe(map((res) => res));
   }
 }
